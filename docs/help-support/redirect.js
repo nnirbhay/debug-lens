7325764278a01721ab853ea72e25f7691d5fc41b
nnirbhay <naviknirbhay@gmail.com>
2025-05-23 00:39:59 +0530

    Fix icon path in Privacy Policy page and improve redirect function logging

--- docs/help-support/redirect.js
+++ docs/help-support/redirect.js
@@ -4,10 +4,12 @@ const pageBaseURL = 'https://nnirbhay.github.io/debug-lens/'
 function navigateToPage(){
     let params = new URLSearchParams(document.location.search);
     let docId = params.get('docId');
-
+    console.log('params : ', params);
+    console.log('docId : ', docId);
+    
     switch (docId) {
         case 'privacy-policy':
-            window.location.href = pageBaseURL+'privacy-policy'
+            window.location = pageBaseURL+'privacy-policy'
             break;
     
         default:
