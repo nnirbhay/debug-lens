075c927e294f1460a9134a3fc29cb1d68415c5e9
nnirbhay <naviknirbhay@gmail.com>
2025-05-23 00:52:53 +0530

    - Change  privacy-policy.html with structured content outlining data collection, usage, and user rights. - Added privacy-policy.css for styling the privacy policy page, including responsive design and dark mode support.

--- docs/help-support/redirect.js
+++ docs/help-support/redirect.js
@@ -4,8 +4,6 @@ navigateToPage()
 function navigateToPage(){
     let params = new URLSearchParams(document.location.search);
     let docId = params.get('docId');
-    console.log('params : ', params);
-    console.log('docId : ', docId);
     
     switch (docId) {
         case 'privacy-policy':
