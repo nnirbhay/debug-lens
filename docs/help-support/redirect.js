65ba263526c6f0e78697ff6a84767f9d508efcd9
nnirbhay <naviknirbhay@gmail.com>
2025-05-23 00:43:38 +0530

    Call navigateToPage function on script load

--- docs/help-support/redirect.js
+++ docs/help-support/redirect.js
@@ -1,6 +1,6 @@
 const pageBaseURL = 'https://nnirbhay.github.io/debug-lens/'
 
-
+navigateToPage()
 function navigateToPage(){
     let params = new URLSearchParams(document.location.search);
     let docId = params.get('docId');
